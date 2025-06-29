55922d64092e043a299a62b0ba140e5f52907e34
Jay <varrajayasimha@gmail.com>
2025-06-29 22:51:54 +0530

    Variables changed to runtime arguments

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I'm fine, How are you doing"
+echo "$PERSON1: I'm fine too, Whats up?"
+echo "$PERSON2: Nothing, Just going to MARS, will you come?"
+echo "$PERSON1: Sorry, you carry on!, I will come once you are back"
