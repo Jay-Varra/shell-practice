1521cc52267746b189026761315e3a191de6d951
Jay <varrajayasimha@gmail.com>
2025-06-29 22:38:54 +0530

    varibles example

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=TRUMP
+PERSON2=MUSK
+
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I'm fine, How are you doing"
+echo "$PERSON1: I'm fine too, Whats up?"
+echo "$PERSON2: Nothing, Just going to MARS, will you come?"
+echo "$PERSON1: Sorry, you carry on!, I will come once you are back"
